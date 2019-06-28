ccfb064b00c69aa579fac23f0bfbd4cc74c676e2
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-28 13:48:18 +0100

    updated wildfly script

--- wildfly_setup/setup.sh
+++ wildfly_setup/setup.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
 
-ssh admin123@IP(VM2)
+#ssh -q -L 8082:localhost:8082
+#jq install
+sudo apt install -y jq
 
-git clone https://github.com/DWONeill18/devOps_jenkinsSetup.git
+#maybe install java on here?
 
-cd devOps_jenkinsSetup
-./install.sh
-./update.sh
-
+#git clone https://github.com/DWONeill18/devOps_jenkinsSetup.git
+#cd devOps_jenkinsSetup
+#./install.sh
+#./update.sh
+sudo apt install wget
+sudo apt install -y wget vim openjdk-8-jdk openjdk-8-jre
 sudo apt-get install maven
 mvn -version
 sudo apt install curl
-curl jnekfjnkef/wildfly.tar.gz -o wildfly.tar.gz
+
+curl https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.tar.gz -o wildfly.tar.gz
 tar -xvf wildfly.tar.gz
 
-scp admin123@52.151.102.180:/home/jenkins/jenkins.war /home/admin123/wildfly-10.1.0.Final/standalone/deployments/
-
-curl https://repo1.maven.org../h2-1.4.197.jar -o h2-1.4.197.jar
-
-java -jar h2-1.4.197.jar &
 
 
+#install azure
+curl -L https://aka.ms/InstallAzureCli | bash
+az login
+#install azure
+
+
+vm2_ip = $(az network public-ip show --name publicip2 | jq -r '.ipAddress')
+echo $vm2_ip
+
+#az vm show -name -q -L 8082:localhost:8082
+
+
+
+
+#Need to sort out keys first
+
+#scp user@ip:/home/jenkins/jenkins.war /home/user2/wildfly-10.1.0.Final/standalone/deployments/
+
+curl https://repo1.maven.org/maven2/com/h2database/h2/1.4.197/h2-1.4.197.jar -o h2-1.4.197.jar
+
+#java -jar h2-1.4.197.jar -webAllowOthers &
+
+#./standalone.sh -b=0.0.0.0
